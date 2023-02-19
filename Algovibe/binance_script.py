fdfc942c8d56cb1435914d2de41c4ae00a0fe0f3
dcapps4140 <62002969+dcapps4140@users.noreply.github.com>
2023-02-19 05:38:07 -0600

    Correcting logic in way data is gathered

--- Algovibe/binance_script.py
+++ Algovibe/binance_script.py
@@ -1,45 +1,57 @@
 import json
 import os
-import websocket
+import asyncio
+import ta
+import websockets
 import pandas as pd
 from binance.client import Client
 
+# Get the value of an environment variable
+API_KEY = os.environ.get('API_KEY')
+API_KEY_SK = os.environ.get('API_KEY_SK')
 
-API_KEY = 'QiUbdlK0KehGdgOJxokt7rO2OwGP8wjAQkHb8mVeGmgkccmgauOFmn5TV3tX8FMS'
-API_KEY_SK = 'NgQTQjNNnBbD8WMWR3MTRlb5aEgkk4rRabKCOzFZdJIL16tibgxJEc8c9yrZelKu'
+df = pd.DataFrame()
+df_list = []  # initialize an empty list to store DataFrames
+open_position = False
 
 client = Client(API_KEY, API_KEY_SK, tld='us')
 
-END_POINT = 'wss://stream.binance.us:9443/ws'
+stream = websockets.connect('wss://testnet.binance.vision/stream?streams=btcusdt@ticker')
 
-our_msg = json.dumps({'method':'SUBSCRIBE', 'params':['btcusdt@ticker'],'id':1})
+def createframe(msg):
+    df = pd.DataFrame([msg])
+    df = df.loc[:,['s','E','c']]
+    df.columns = ['symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    return df
 
-df = pd.DataFrame()
-IN_POSITION = False
-BUY_ORDERS, SELL_ORDERS = [], []
+async def main():
+    global df, df_list, open_position, open_position
+    async with stream as receiver:
+        while not should_stop: # use a global variable to determine when to stop
+            data = await receiver.rec()
+            data = json.loads(data)['data']
+            new_df = createframe(data)
+            df_list.append(new_df)
+            df = pd.concat(df_list, ignore_index=True)
+            if len(df) > 30:
+                if not open_position:
+                    if ta.momentum.roc(df.Price, 30).iloc[-1] > 0 and ta.momentum.roc(df.Price, 30).iloc[-2]:
+                        print('bought for '+ str(last_price))
+                        BUY_ORDERS.append(last_price)
+                        open_position = True
+                # if open_position and sma_5 > last_price:
+                #     print('sold for '+ str(last_price))
+                #     print('profit: '+ str(last_price - BUY_ORDERS[-1]))
+                #     SELL_ORDERS.append(last_price)
+                #     open_position = False
 
-def on_open(ws):
-    '''This is the callback function that will be called when the connection is opened'''
-    ws.send(our_msg)
-
-def on_message(ws, message):
-    global	df, IN_POSITION, BUY_ORDERS, SELL_ORDERS
-    out = json.loads(message)
-    out = pd.DataFrame({'price': [float(out['c'])]}, index=[pd.to_datetime(out['E'], units='ms')])
-    df = pd.concat([df,out],axis=0)
-    print(df)
-    df = df.tail(5)
-    last_price = df.tail(1).price.values[0]
-    sma_5 = df.price.rolling(5).mean().tail(1).values[0]
-    if not IN_POSITION and last_price > sma_5:
-        print('bought for '+ str(last_price))
-        BUY_ORDERS.append(last_price)
-        IN_POSITION = True
-    if IN_POSITION and sma_5 > last_price:
-        print('sold for '+ str(last_price))
-        print('profit: '+ str(last_price - BUY_ORDERS[-1]))
-        SELL_ORDERS.append(last_price)
-        IN_POSITION = False
-
-ws = websocket.WebSocketApp(END_POINT, on_message=on_message, on_open=on_open)
-ws.run_forever()
+# initialize the kill switch
+should_stop = False
+            
+if __name__ == '__main__':
+    try:
+        asyncio.get_event_loop().run_until_complete(main()) # run the main function until it is complete
+    except KeyboardInterrupt:
+        should_stop = True # set the kill switch to stop the loop
